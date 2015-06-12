9efa605f5a6d63a723e3ec5b41b85f60448af534
TakeAsh <take@TakeAsh.net>
2015-06-13 02:05:36 +0900

    add ResourceHelper, RibbonExtensionMethods

---
+++ WpfUtility/ResourceHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace WpfUtility {
+
+    public static class ResourceHelper {
+
+        public static BitmapImage GetImage(string filename) {
+            if (filename == null) {
+                return null;
+            }
+            var assembly = Assembly.GetCallingAssembly().GetName().Name;
+            return new BitmapImage(new Uri("/" + assembly + ";component/" + filename, UriKind.Relative));
+        }
+
+        /// <summary>
+        /// Get text of embedded resource file
+        /// </summary>
+        /// <param name="filename">resource file name</param>
+        /// <param name="encoding">encoding of file content. Default is UTF8.</param>
+        /// <returns>content of resource file</returns>
+        /// <remarks>The resource file must be embedded.</remarks>
+        public static string GetText(string filename, Encoding encoding = null) {
+            if (filename == null) {
+                return null;
+            }
+            encoding = encoding ?? Encoding.UTF8;
+            var assembly = Assembly.GetCallingAssembly();
+            var directory = Path.GetDirectoryName(filename).Replace('-', '_');
+            var file = Path.GetFileName(filename);
+            var resourceName = Path.Combine(assembly.GetName().Name, directory, file).Replace('\\', '.').Replace('/', '.');
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream, encoding)) {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ WpfUtility/RibbonExtensionMethods.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Microsoft.Windows.Controls.Ribbon;
+
+namespace WpfUtility {
+
+    public static class RibbonExtensionMethods {
+
+        const string DefaultShowIcon = "Images/Show.png";
+        const string DefaultHideIcon = "Images/Hide.png";
+
+        public static T AddHelpItem<T>(
+            this Ribbon ribbon,
+            T item
+        )
+            where T : UIElement {
+
+            var helpPanel = (ribbon.HelpPaneContent as StackPanel) ?? new StackPanel() {
+                Orientation = Orientation.Horizontal,
+            };
+            helpPanel.Children.Add(item);
+            ribbon.HelpPaneContent = helpPanel;
+            return item;
+        }
+
+        public static RibbonButton AddMinimizeButton(
+            this Ribbon ribbon,
+            BitmapImage showIcon = null,
+            BitmapImage hideIcon = null,
+            string showToolTip = null,
+            string hideToolTip = null
+        ) {
+            showIcon = showIcon ?? ResourceHelper.GetImage(DefaultShowIcon);
+            hideIcon = hideIcon ?? ResourceHelper.GetImage(DefaultHideIcon);
+            showToolTip = showToolTip ?? Properties.Resources.RibbonExtensionMethods_MinimizeButton_ToolTip_Show;
+            hideToolTip = hideToolTip ?? Properties.Resources.RibbonExtensionMethods_MinimizeButton_ToolTip_Hide;
+
+            var button = new RibbonButton() {
+                SmallImageSource = hideIcon,
+                ToolTip = hideToolTip,
+            };
+            button.Click += (s, e) => {
+                ribbon.IsMinimized = !ribbon.IsMinimized;
+            };
+            // ribbon.IsMinimizedChanged += ...
+            DependencyPropertyDescriptor
+                .FromProperty(Ribbon.IsMinimizedProperty, typeof(Ribbon))
+                .AddValueChanged(ribbon, (s, e) => {
+                    button.SmallImageSource = ribbon.IsMinimized ?
+                        showIcon :
+                        hideIcon;
+                    button.ToolTip = ribbon.IsMinimized ?
+                        showToolTip :
+                        hideToolTip;
+                });
+            return ribbon.AddHelpItem(button);
+        }
+    }
+}
