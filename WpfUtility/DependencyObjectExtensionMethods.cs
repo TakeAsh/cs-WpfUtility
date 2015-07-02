875af2cee76fffe2a803703254fe9007cdffd7bf
TakeAsh <take@TakeAsh.net>
2015-07-02 17:44:18 +0900

    add DependencyObjectExtensionMethods
    - FindName<T>()

---
+++ WpfUtility/DependencyObjectExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace WpfUtility {
+
+    public static class DependencyObjectExtensionMethods {
+
+        public static T FindName<T>(this DependencyObject obj, string name)
+            where T : class {
+
+            return LogicalTreeHelper.FindLogicalNode(obj, name) as T;
+        }
+    }
+}
