eb05e5224084ade446e040f9e589656ae0dafb51
TakeAsh <take@TakeAsh.net>
2015-07-03 07:11:24 +0900

    change DependencyObjectExtensionMethods
    - change method name: FindName<T>() -> FindChild<T>()
    - add null check.
    - add comment.

--- WpfUtility/DependencyObjectExtensionMethods.cs
+++ WpfUtility/DependencyObjectExtensionMethods.cs
@@ -8,10 +8,22 @@ namespace WpfUtility {
 
     public static class DependencyObjectExtensionMethods {
 
-        public static T FindName<T>(this DependencyObject obj, string name)
+        /// <summary>
+        /// Attempts to find and return an object that has the specified name. The search starts from the specified object and continues into subnodes of the logical tree.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to find.</typeparam>
+        /// <param name="node">The object to start searching from. This object must be either a FrameworkElement or a FrameworkContentElement.</param>
+        /// <param name="name">The name of the object to find.</param>
+        /// <returns>The object with the matching name, if one is found; returns null if no matching name was found in the logical tree.</returns>
+        /// <remarks>
+        /// [LogicalTreeHelper.FindLogicalNode Method (System.Windows)](https://msdn.microsoft.com/en-us/library/system.windows.logicaltreehelper.findlogicalnode.aspx)
+        /// </remarks>
+        public static T FindChild<T>(this DependencyObject node, string name)
             where T : class {
 
-            return LogicalTreeHelper.FindLogicalNode(obj, name) as T;
+            return node != null ?
+                LogicalTreeHelper.FindLogicalNode(node, name) as T :
+                null;
         }
     }
 }
