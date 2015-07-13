0093c78f2e621042b73e57fb332d81594062e8f4
TakeAsh <take@TakeAsh.net>
2015-07-13 22:39:28 +0900

    add ItemCollectionExtensionMethods
    - Add<T>(this ItemCollection items, IEnumerable<UIElement> list)

---
+++ WpfUtility/ItemCollectionExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfUtility {
+
+    public static class ItemCollectionExtensionMethods {
+
+        public static void Add<T>(this ItemCollection items, IEnumerable<UIElement> list)
+            where T : UIElement {
+
+            if (items == null || list == null) {
+                return;
+            }
+            list.ToList()
+                .ForEach(item => items.Add(item));
+        }
+    }
+}
