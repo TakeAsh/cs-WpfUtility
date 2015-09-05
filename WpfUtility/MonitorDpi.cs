7c0be5362e207f167836dbf94326c6b3bbcbb626
TakeAsh <take@TakeAsh.net>
2015-09-05 23:03:39 +0900

    add MonitorDpi

---
+++ WpfUtility/MonitorDpi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfUtility {
+
+    public class MonitorDpi {
+
+        public const double DefaultDpi = 96;
+
+        private PresentationSource _source;
+
+        public MonitorDpi(Visual visual) {
+            _source = PresentationSource.FromVisual(visual);
+            if (_source == null) {
+                throw new ArgumentException("Failed to get a PresentationSource from the visual.");
+            }
+            Update();
+        }
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public override string ToString() {
+            return "X:" + X + ", Y:" + Y;
+        }
+
+        public void Update() {
+            X = DefaultDpi * _source.CompositionTarget.TransformToDevice.M11;
+            Y = DefaultDpi * _source.CompositionTarget.TransformToDevice.M22;
+        }
+    }
+}
