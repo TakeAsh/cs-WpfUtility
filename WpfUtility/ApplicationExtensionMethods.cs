00dd82ae43d5692a8692230662c2cf35d26dfac4
TakeAsh <take@TakeAsh.net>
2015-11-09 21:04:26 +0900

    add ApplicationExtensionMethods
    - public static T SafeTryFindResource<T>(this Application app, object
    key, T defaultValue = default(T))

---
+++ WpfUtility/ApplicationExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using TakeAshUtility;
+
+namespace WpfUtility {
+
+    public static class ApplicationExtensionMethods {
+
+        public static T SafeTryFindResource<T>(this Application app, object key, T defaultValue = default(T)) {
+            return app == null ?
+                defaultValue :
+                app.TryFindResource(key)
+                    .SafeToObject(defaultValue);
+        }
+    }
+}
