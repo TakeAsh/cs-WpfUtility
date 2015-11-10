285b5e3e611dcf817e2bf2837e842bd5f6bc85de
TakeAsh <take@TakeAsh.net>
2015-11-10 13:57:02 +0900

    add PointCollectionExtensionMethods
    - public static PathFigure ToPathFigure(this PointCollection points,
    bool close = false)

---
+++ WpfUtility/PointCollectionExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace WpfUtility {
+
+    public static class PointCollectionExtensionMethods {
+
+        public static PathFigure ToPathFigure(this PointCollection points, bool close = false) {
+            if (points == null || points.Count < 2) {
+                return null;
+            }
+            PointCollection workPoints;
+            if (close) {
+                workPoints = points.Clone();
+                workPoints.Add(workPoints.First());
+            } else {
+                workPoints = points;
+            }
+            return new PathFigure() {
+                StartPoint = workPoints.First(),
+                Segments = { new PolyLineSegment(workPoints.Skip(1), true) },
+            };
+        }
+    }
+}
