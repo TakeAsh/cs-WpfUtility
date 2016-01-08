0ee12256f8d12a8bbe601a313364dd7ae2efb36d
TakeAsh <take@TakeAsh.net>
2016-01-08 16:12:34 +0900

    add IMessageButtonHost
    - If a object has an IMessageButtonHost property, it can show message
    through the property.

---
+++ WpfUtility/IMessageButtonHost.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfUtility {
+
+    public interface IMessageButtonHost {
+
+        MessageButton.Icons MessageIcon { get; set; }
+
+        string MessageText { get; set; }
+
+        void ShowMessage(string text, MessageButton.Icons icon = MessageButton.Icons.Beep);
+
+        void ShowMessage(IEnumerable<string> list, MessageButton.Icons icon = MessageButton.Icons.Beep);
+    }
+
+    public static class IMessageButtonHostExtensionMethods {
+
+        public static void SafeShowMessage(
+            this IMessageButtonHost host,
+            string text,
+            MessageButton.Icons icon = MessageButton.Icons.Beep
+        ) {
+            if (host == null) {
+                return;
+            }
+            host.ShowMessage(text, icon);
+        }
+
+        public static void SafeShowMessage(
+            this IMessageButtonHost host,
+            IEnumerable<string> list,
+            MessageButton.Icons icon = MessageButton.Icons.Beep
+        ) {
+            if (host == null) {
+                return;
+            }
+            host.ShowMessage(list, icon);
+        }
+    }
+}
