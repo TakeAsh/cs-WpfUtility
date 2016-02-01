518f06ed6e049b32c3e6c0258e49a9221cb35621
TakeAsh <take@TakeAsh.net>
2016-02-02 05:19:48 +0900

    add BrushExtensionMethods
    - public static Material ToMaterial(this Brush brush)

---
+++ WpfUtility/BrushExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace WpfUtility {
+
+    public static class BrushExtensionMethods {
+
+        public static Material ToMaterial(this Brush brush) {
+            return new DiffuseMaterial(brush);
+        }
+    }
+}
