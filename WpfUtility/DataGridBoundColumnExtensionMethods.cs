1ddb815f01dca929421a263d894ce04045cd3d69
TakeAsh <take@TakeAsh.net>
2016-02-16 03:39:29 +0900

    WpfUtility: add DataGridBoundColumnExtensionMethods
    - public static Style getCurrentStyle(this DataGridBoundColumn column,
    Type type)

---
+++ WpfUtility/DataGridBoundColumnExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using TakeAshUtility;
+
+namespace WpfUtility {
+
+    public static class DataGridBoundColumnExtensionMethods {
+
+        public static Style getCurrentStyle(
+            this DataGridBoundColumn column,
+            Type type
+        ) {
+            return column.ElementStyle.Setters.Aggregate(
+                new Style(type),
+                (current, setter) => {
+                    current.Setters.Add(setter);
+                    return current;
+                }
+            );
+        }
+    }
+}
