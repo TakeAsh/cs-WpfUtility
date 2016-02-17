d652b95cd3819245012a206c283571877f9740c6
TakeAsh <take@TakeAsh.net>
2016-02-18 01:18:49 +0900

    add Model3DExtensionMethods
    - public static object GetTag(this Model3D model)
    - public static void SetTag(this Model3D model, object value)

---
+++ WpfUtility/Model3DExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace WpfUtility {
+
+    public static class Model3DExtensionMethods {
+
+        public static object GetTag(this Model3D model) {
+            return model.GetValue(FrameworkElement.TagProperty);
+        }
+
+        public static void SetTag(this Model3D model, object value) {
+            model.SetValue(FrameworkElement.TagProperty, value);
+        }
+    }
+}
