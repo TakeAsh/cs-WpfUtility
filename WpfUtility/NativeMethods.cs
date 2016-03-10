3ca6d4a15b3db19d44f3270b13b9c16d57f5915f
TakeAsh <take@TakeAsh.net>
2016-03-10 19:38:05 +0900

    NativeMethods: add ToIntUnchecked(), GetLowWord(), GetHighWord()
    - public static int ToIntUnchecked(this IntPtr value)
    - public static int GetLowWord(this IntPtr value)
    - public static int GetHighWord(this IntPtr value)

--- WpfUtility/NativeMethods.cs
+++ WpfUtility/NativeMethods.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 
 namespace WpfUtility.Native {
 
-    class NativeMethods {
+    static class NativeMethods {
 
         [DllImport("user32.dll")]
         public static extern bool SetWindowPlacement(
@@ -19,5 +19,37 @@ namespace WpfUtility.Native {
             IntPtr hWnd,
             out WINDOWPLACEMENT lpwndpl
         );
+
+        /// <summary>
+        /// Convert a value to int.
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Int value</returns>
+        /// <remarks>
+        /// [winapi - Win api in C#. Get Hi and low word from IntPtr - Stack Overflow](http://stackoverflow.com/questions/7913325/)
+        /// </remarks>
+        public static int ToIntUnchecked(this IntPtr value) {
+            return IntPtr.Size == 8 ?
+                unchecked((int)value.ToInt64()) :
+                value.ToInt32();
+        }
+
+        /// <summary>
+        /// Get low word of a value.
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>low word of a value</returns>
+        public static int GetLowWord(this IntPtr value) {
+            return unchecked((short)value.ToIntUnchecked());
+        }
+
+        /// <summary>
+        /// Get high word of a value.
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>high word of a value</returns>
+        public static int GetHighWord(this IntPtr value) {
+            return unchecked((short)(((uint)value.ToIntUnchecked()) >> 16));
+        }
     }
 }
