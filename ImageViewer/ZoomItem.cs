d98cd89a0ebea3d99c708095d72d478b411ee96e
TakeAsh <take@TakeAsh.net>
2016-03-15 00:14:46 +0900

    ImageViewer: implement IEquatable in ZoomItem

--- ImageViewer/ZoomItem.cs
+++ ImageViewer/ZoomItem.cs
@@ -6,7 +6,8 @@ using TakeAshUtility;
 
 namespace ImageViewer {
 
-    public struct ZoomItem {
+    public struct ZoomItem :
+        IEquatable<ZoomItem> {
 
         public ZoomItem(double value)
             : this() {
@@ -22,6 +23,36 @@ namespace ImageViewer {
         public override string ToString() {
             return Title;
         }
+
+        #region IEquatable
+
+        public bool Equals(ZoomItem other) {
+            if (ReferenceEquals(this, other)) {
+                return true;
+            }
+            return this.Value == other.Value;
+        }
+
+        public override bool Equals(object other) {
+            if (other == null || !(other is ZoomItem)) {
+                return false;
+            }
+            return this.Equals((ZoomItem)other);
+        }
+
+        public override int GetHashCode() {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(ZoomItem x, ZoomItem y) {
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(ZoomItem x, ZoomItem y) {
+            return !x.Equals(y);
+        }
+
+        #endregion
     }
 
     public static class ZoomItemExtensionMethods {
