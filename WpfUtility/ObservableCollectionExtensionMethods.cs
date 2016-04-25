50fab16b93f30e8f291d3dbefd009b20df1b9c41
TakeAsh <take@TakeAsh.net>
2016-04-25 18:04:48 +0900

    add ObservableCollectionExtensionMethods
    - public static ICollectionView GetDefaultView<T>(this
    ObservableCollection<T> collection)
    - public static void SortBy(this ICollectionView view, string
    propertyName, ListSortDirection direction = ListSortDirection.Ascending)

---
+++ WpfUtility/ObservableCollectionExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace WpfUtility {
+
+    public static class ObservableCollectionExtensionMethods {
+
+        public static ICollectionView GetDefaultView<T>(this ObservableCollection<T> collection) {
+            if (collection == null) {
+                return null;
+            }
+            return CollectionViewSource.GetDefaultView(collection);
+        }
+
+        public static void SortBy(
+            this ICollectionView view,
+            string propertyName,
+            ListSortDirection direction = ListSortDirection.Ascending
+        ) {
+            if (view == null) {
+                return;
+            }
+            view.SortDescriptions.Add(new SortDescription(propertyName, direction));
+        }
+    }
+}
