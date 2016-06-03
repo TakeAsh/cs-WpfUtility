9a8c0bfd471fd3b239be1956ae548620f4d7f547
TakeAsh <take@TakeAsh.net>
2016-06-03 18:51:41 +0900

    add FrameworkElementExtensionMethods
    - public static T SafeTryFindResource<T>(this FrameworkElement element,
    object key, T defaultValue = default(T))

---
+++ WpfUtility/FrameworkElementExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using TakeAshUtility;
+
+namespace WpfUtility {
+
+    public static class FrameworkElementExtensionMethods {
+
+        public static T SafeTryFindResource<T>(
+            this FrameworkElement element,
+            object key,
+            T defaultValue = default(T)
+        ) {
+            return element == null ?
+                defaultValue :
+                element.TryFindResource(key)
+                    .SafeToObject(defaultValue);
+        }
+    }
+}
