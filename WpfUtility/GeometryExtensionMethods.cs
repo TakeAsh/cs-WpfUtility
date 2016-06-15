3473b5c017d24eee5bfcd72c34671d6fb0b95d1d
TakeAsh <take@TakeAsh.net>
2016-06-15 11:41:46 +0900

    add GeometryExtensionMethods
    - public static GeometryCollection ToGeometryCollection(this
    IEnumerable<Geometry> source)
    - public static GeometryGroup ToGeometryGroup(this GeometryCollection
    source)
    - public static GeometryGroup ToGeometryGroup(this IEnumerable<Geometry>
    source)

---
+++ WpfUtility/GeometryExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using TakeAshUtility;
+
+namespace WpfUtility {
+
+    public static class GeometryExtensionMethods {
+
+        public static GeometryCollection ToGeometryCollection(this IEnumerable<Geometry> source) {
+            if (source.SafeCount() == 0) {
+                return null;
+            }
+            return new GeometryCollection(source);
+        }
+
+        public static GeometryGroup ToGeometryGroup(this GeometryCollection source) {
+            if (source.SafeCount() == 0) {
+                return null;
+            }
+            return new GeometryGroup() {
+                Children = source,
+            };
+        }
+
+        public static GeometryGroup ToGeometryGroup(this IEnumerable<Geometry> source) {
+            if (source.SafeCount() == 0) {
+                return null;
+            }
+            return new GeometryGroup() {
+                Children = new GeometryCollection(source),
+            };
+        }
+    }
+}
