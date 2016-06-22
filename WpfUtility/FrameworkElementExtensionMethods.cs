851e7a567183619c4a7b8ea1576dca64f2e2f7f6
TakeAsh <take@TakeAsh.net>
2016-06-22 09:49:24 +0900

    FrameworkElementExtensionMethods: add SafeGetTag<T>()
    - public static T SafeGetTag<T>(this FrameworkElement element, T
    defaultValue = default(T))

--- WpfUtility/FrameworkElementExtensionMethods.cs
+++ WpfUtility/FrameworkElementExtensionMethods.cs
@@ -19,5 +19,14 @@ namespace WpfUtility {
                 element.TryFindResource(key)
                     .SafeToObject(defaultValue);
         }
+
+        public static T SafeGetTag<T>(
+            this FrameworkElement element,
+            T defaultValue = default(T)
+        ) {
+            return element == null || element.Tag == null || !(element.Tag is T) ?
+                defaultValue :
+                (T)element.Tag;
+        }
     }
 }
