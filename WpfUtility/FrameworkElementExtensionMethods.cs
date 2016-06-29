6c871602664695637d26b93c62d315264d630b3a
TakeAsh <take@TakeAsh.net>
2016-06-29 11:19:13 +0900

    FrameworkElementExtensionMethods: add UpdateLayoutEx()
    - public static void UpdateLayoutEx(this FrameworkElement element)

--- WpfUtility/FrameworkElementExtensionMethods.cs
+++ WpfUtility/FrameworkElementExtensionMethods.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Threading;
 using TakeAshUtility;
 
 namespace WpfUtility {
@@ -27,6 +28,26 @@ namespace WpfUtility {
             return element == null || element.Tag == null || !(element.Tag is T) ?
                 defaultValue :
                 (T)element.Tag;
+        }
+
+        /// <summary>
+        /// Update the layout of the element according to its Width and Height.
+        /// </summary>
+        /// <param name="element">the FrameworkElement to be updated.</param>
+        public static void UpdateLayoutEx(this FrameworkElement element) {
+            element.Dispatcher.Invoke(
+                DispatcherPriority.Render,
+                new Action(() => {
+                    if (element == null ||
+                        double.IsNaN(element.Width) || double.IsNaN(element.Height) ||
+                        double.IsInfinity(element.Width) || double.IsInfinity(element.Height)) {
+                        return;
+                    }
+                    element.Measure(new Size(element.Width, element.Height));
+                    element.Arrange(new Rect(0, 0, element.Width, element.Height));
+                    element.UpdateLayout();
+                })
+            );
         }
     }
 }
