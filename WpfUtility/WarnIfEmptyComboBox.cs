33f8154a9c5f3c336cc00df3724eb65d6fcb933d
TakeAsh <take@TakeAsh.net>
2025-02-16 19:30:48 +0900

    WarnIfEmptyComboBox: fix: not work when IsEditable == false

--- WpfUtility/WarnIfEmptyComboBox.cs
+++ WpfUtility/WarnIfEmptyComboBox.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
+using TakeAshUtility;
 
 namespace WpfUtility {
 
@@ -15,9 +16,12 @@ namespace WpfUtility {
     /// </summary>
     /// <see cref="https://stackoverflow.com/questions/36177619/">c# - Change the background color of a WPF editable ComboBox programmatically - Stack Overflow</see>
     /// <see cref="https://stackoverflow.com/questions/70958379/">xaml - Change background colour of a ComboBox in WPF without pasting entire template? - Stack Overflow</see>
+    /// <see cref="http://tawamuredays.blog.fc2.com/blog-entry-280.html">ComboBoxの背景色が変わらない - C#等と戯れる日々</see>
     public class WarnIfEmptyComboBox : ComboBox {
 
         public Brush WarnBrush { get; set; } = Brushes.LightPink;
+        private Border border = null;
+        private Brush originalBackground = null;
 
         public WarnIfEmptyComboBox() : base() {
             this.Loaded += OnLoaded;
@@ -27,22 +31,28 @@ namespace WpfUtility {
             var comboBox = sender as ComboBox;
             if (comboBox == null) { return; }
             if (comboBox.IsEditable) {
-                var textbox = comboBox?.Template?.FindName("PART_EditableTextBox", comboBox) as TextBox;
-                if (textbox == null) { return; }
+                var textbox = comboBox.Template?.FindName("PART_EditableTextBox", comboBox) as TextBox;
+                border = textbox?.Parent as Border;
+                if (textbox == null || border == null) { return; }
+                originalBackground = border.Background;
                 textbox.TextChanged += OnEditableTextChanged;
                 OnEditableTextChanged(textbox);
             } else {
+                var toggleButton = comboBox.Template.FindName("toggleButton", comboBox) as ToggleButton;
+                border = toggleButton?.Template.FindName("templateRoot", toggleButton) as Border;
+                if (toggleButton == null || border == null) { return; }
+                originalBackground = border.Background;
                 comboBox.SelectionChanged += OnSelectionChanged;
+                OnSelectionChanged(comboBox);
             }
         }
 
         private void OnEditableTextChanged(object sender, TextChangedEventArgs e = null) {
             var textbox = sender as TextBox;
-            var border = textbox?.Parent as Border;
-            if (textbox == null || border == null) { return; }
+            if (textbox == null) { return; }
             border.Background = String.IsNullOrEmpty(textbox.Text)
                 ? WarnBrush
-                : null;
+                : originalBackground;
         }
 
         private void OnSelectionChanged(object sender, SelectionChangedEventArgs e = null) {
@@ -55,9 +65,7 @@ namespace WpfUtility {
                 || (content = comboBoxItem.Content) == null
                 || String.IsNullOrEmpty(content.ToString())
                 || String.IsNullOrEmpty(content as string);
-            var root = comboBox.Template?.FindName("templateRoot", comboBox) as Grid;
-            if (root == null) { return; }
-            root.Background = isEmpty ? WarnBrush : null;   // not work now
+            border.Background = isEmpty ? WarnBrush : originalBackground;
         }
     }
 }
