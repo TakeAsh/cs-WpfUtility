a74d8e567597615e4052b5a071f006a2636f5dc2
TakeAsh <take@TakeAsh.net>
2025-03-08 14:19:55 +0900

    Placeholder: store PlaceholderBrush as private property and reuse it

--- WpfUtility/PlaceHolder.cs
+++ WpfUtility/PlaceHolder.cs
@@ -30,22 +30,50 @@ namespace WpfUtility {
             new PropertyMetadata(null, OnPlaceHolderChanged)
         );
 
+        private static readonly DependencyProperty IsInitializedProperty = DependencyProperty.RegisterAttached(
+            "IsInitialized",
+            typeof(bool),
+            typeof(Placeholder)
+        );
+
+        private static readonly DependencyProperty OriginalBackgroundProperty = DependencyProperty.RegisterAttached(
+            "OriginalBackground",
+            typeof(Brush),
+            typeof(Placeholder)
+        );
+
+        private static readonly DependencyProperty PlaceholderBrushProperty = DependencyProperty.RegisterAttached(
+            "PlaceholderBrush",
+            typeof(Brush),
+            typeof(Placeholder)
+        );
+
         private static void OnPlaceHolderChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) {
             var placeHolder = e.NewValue as string;
-            var textChangedHandler = CreateTextChangedEventHandler(placeHolder);
-            var selectionChangedHandler = CreateSelectionChangedEventHandler(placeHolder);
-            var ribbonGallerySelectionChangedHandler = CreateRibbonGallerySelectionChangedEventHandler(placeHolder);
+            var textChangedHandler = CreateTextChangedEventHandler();
+            var selectionChangedHandler = CreateSelectionChangedEventHandler();
+            var ribbonGallerySelectionChangedHandler = CreateRibbonGallerySelectionChangedEventHandler();
             var textBox = sender as TextBox;
             var comboBox = sender as ComboBox;
             var ribbonComboBox = sender as RibbonComboBox;
             if (textBox != null) {
+                if (!GetIsInitialized(textBox)) {
+                    SetIsInitialized(textBox, true);
+                    SetOriginalBackground(textBox, textBox.Background);
+                }
+                SetPlaceholderBrush(textBox, CreateVisualBrush(placeHolder));
                 if (String.IsNullOrEmpty(placeHolder)) {
                     textBox.TextChanged -= textChangedHandler;
                 } else {
                     textBox.TextChanged += textChangedHandler;
                 }
-                DrawPlaceHolder(textBox, placeHolder, textBox.Text);
+                DrawPlaceHolder(textBox, textBox.Text);
             } else if (comboBox != null) {
+                if (!GetIsInitialized(comboBox)) {
+                    SetIsInitialized(comboBox, true);
+                    SetOriginalBackground(comboBox, comboBox.Background);
+                }
+                SetPlaceholderBrush(comboBox, CreateVisualBrush(placeHolder));
                 if (String.IsNullOrEmpty(placeHolder)) {
                     comboBox.RemoveHandler(TextBox.TextChangedEvent, textChangedHandler);
                     comboBox.SelectionChanged -= selectionChangedHandler;
@@ -53,8 +81,13 @@ namespace WpfUtility {
                     comboBox.AddHandler(TextBox.TextChangedEvent, textChangedHandler);
                     comboBox.SelectionChanged += selectionChangedHandler;
                 }
-                DrawPlaceHolder(comboBox, placeHolder, comboBox.Text);
+                DrawPlaceHolder(comboBox, comboBox.Text);
             } else if (ribbonComboBox != null) {
+                if (!GetIsInitialized(ribbonComboBox)) {
+                    SetIsInitialized(ribbonComboBox, true);
+                    SetOriginalBackground(ribbonComboBox, ribbonComboBox.Background);
+                }
+                SetPlaceholderBrush(ribbonComboBox, CreateVisualBrush(placeHolder));
                 if (String.IsNullOrEmpty(placeHolder)) {
                     ribbonComboBox.RemoveHandler(TextBox.TextChangedEvent, textChangedHandler);
                     ribbonComboBox.RemoveHandler(RibbonGallery.SelectionChangedEvent, ribbonGallerySelectionChangedHandler);
@@ -62,7 +95,7 @@ namespace WpfUtility {
                     ribbonComboBox.AddHandler(TextBox.TextChangedEvent, textChangedHandler);
                     ribbonComboBox.AddHandler(RibbonGallery.SelectionChangedEvent, ribbonGallerySelectionChangedHandler);
                 }
-                DrawPlaceHolder(ribbonComboBox, placeHolder, ribbonComboBox.Text);
+                DrawPlaceHolder(ribbonComboBox, ribbonComboBox.Text);
             }
         }
 
@@ -71,45 +104,40 @@ namespace WpfUtility {
         /// </summary>
         /// <param name="placeHolder">Text that is shown as the place holder</param>
         /// <returns>TextChanged Event Handler</returns>
-        private static TextChangedEventHandler CreateTextChangedEventHandler(string placeHolder) {
+        private static TextChangedEventHandler CreateTextChangedEventHandler() {
             return (sender, e) => {
                 var textBox = sender as TextBox;
                 var comboBox = sender as ComboBox;
                 var ribbonComboBox = sender as RibbonComboBox;
                 if (textBox != null) {
-                    DrawPlaceHolder(textBox, placeHolder, textBox.Text);
+                    DrawPlaceHolder(textBox, textBox.Text);
                 } else if (comboBox != null) {
-                    DrawPlaceHolder(comboBox, placeHolder, comboBox.Text);
+                    DrawPlaceHolder(comboBox, comboBox.Text);
                 } else if (ribbonComboBox != null) {
-                    DrawPlaceHolder(ribbonComboBox, placeHolder, ribbonComboBox.Text);
+                    DrawPlaceHolder(ribbonComboBox, ribbonComboBox.Text);
                 }
             };
         }
 
-        private static SelectionChangedEventHandler CreateSelectionChangedEventHandler(string placeHolder) {
+        private static SelectionChangedEventHandler CreateSelectionChangedEventHandler() {
             return (sender, e) => {
                 var comboBox = sender as ComboBox;
                 var comboBoxItem = comboBox.SelectedItem as ComboBoxItem;
-                if (comboBox == null) {
-                    return;
-                }
-                var text = comboBox.IsEditable && !String.IsNullOrEmpty(comboBox.Text) ?
-                    comboBox.Text :
-                    (comboBoxItem != null ?
-                        comboBoxItem.Content.SafeToString() :
-                        comboBox.SelectedItem.SafeToString());
-                DrawPlaceHolder(comboBox, placeHolder, text);
+                if (comboBox == null) { return; }
+                var text =
+                    comboBox.IsEditable && !String.IsNullOrEmpty(comboBox.Text) ? comboBox.Text :
+                    comboBoxItem != null ? comboBoxItem.Content.SafeToString() :
+                    comboBox.SelectedItem.SafeToString();
+                DrawPlaceHolder(comboBox, text);
             };
         }
 
-        private static RoutedPropertyChangedEventHandler<object> CreateRibbonGallerySelectionChangedEventHandler(string placeHolder) {
+        private static RoutedPropertyChangedEventHandler<object> CreateRibbonGallerySelectionChangedEventHandler() {
             return (sender, e) => {
                 Mouse.Capture(null);
                 var comboBox = sender as RibbonComboBox;
-                if (comboBox == null) {
-                    return;
-                }
-                DrawPlaceHolder(comboBox, placeHolder, comboBox.Text);
+                if (comboBox == null) { return; }
+                DrawPlaceHolder(comboBox, comboBox.Text);
             };
         }
 
@@ -122,18 +150,16 @@ namespace WpfUtility {
         /// <remarks>
         /// [c# - Getting the Background of a UIElement or sender - Stack Overflow](http://stackoverflow.com/questions/16224920)
         /// </remarks>
-        private static void DrawPlaceHolder(UIElement element, string placeHolder, string text) {
+        private static void DrawPlaceHolder(UIElement element, string text) {
             var background = element.GetType().GetProperty("Background");
             var toolTip = element.GetType().GetProperty("ToolTip");
-            if (background == null || toolTip == null) {
-                return;
-            }
+            if (background == null || toolTip == null) { return; }
             if (String.IsNullOrEmpty(text)) {
-                background.SetValue(element, CreateVisualBrush(placeHolder), null);
-                toolTip.SetValue(element, placeHolder, null);
+                background.SetValue(element, GetPlaceholderBrush(element), null);
+                toolTip.SetValue(element, GetText(element), null);
             } else {
-                background.SetValue(element, new SolidColorBrush(Colors.White), null);
-                toolTip.SetValue(element, placeHolder + ":\n" + text, null);
+                background.SetValue(element, GetOriginalBackground(element), null);
+                toolTip.SetValue(element, GetText(element) + ":\n" + text, null);
             }
         }
 
@@ -142,8 +168,8 @@ namespace WpfUtility {
                 Text = placeHolder,
                 Padding = new Thickness(5, 1, 1, 1),
                 Margin = new Thickness(10),
-                Foreground = new SolidColorBrush(Colors.Gray),
-                //Background = new SolidColorBrush(Colors.White),
+                Foreground = Brushes.Gray,
+                //Background = Brushes.White,
                 HorizontalAlignment = HorizontalAlignment.Left,
                 VerticalAlignment = VerticalAlignment.Center,
             };
@@ -161,6 +187,30 @@ namespace WpfUtility {
 
         public static string GetText(UIElement element) {
             return element.GetValue(TextProperty) as string;
+        }
+
+        private static void SetIsInitialized(UIElement element, bool initialized) {
+            element.SetValue(IsInitializedProperty, initialized);
+        }
+
+        private static bool GetIsInitialized(UIElement element) {
+            return (bool)element.GetValue(IsInitializedProperty);
+        }
+
+        private static void SetOriginalBackground(UIElement element, Brush brush) {
+            element.SetValue(OriginalBackgroundProperty, brush);
+        }
+
+        private static Brush GetOriginalBackground(UIElement element) {
+            return element.GetValue(OriginalBackgroundProperty) as Brush;
+        }
+
+        private static void SetPlaceholderBrush(UIElement element, Brush brush) {
+            element.SetValue(PlaceholderBrushProperty, brush);
+        }
+
+        private static Brush GetPlaceholderBrush(UIElement element) {
+            return element.GetValue(PlaceholderBrushProperty) as Brush;
         }
     }
 }
